524eb05f9432ed5f4150db598e2e912510a4c2aa
nicolajhansen97 <nicolajhansen97@live.dk>
2021-05-31 14:31:35 +0200

    Interface update

---
+++ src/loadInterface.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public interface loadInterface {
+    public void getFilepath();
+
+    public ArrayList loadTextFile();
+
+    public void getName();
+
+
+}
---
+++ src/saveInterface.java
@@ -0,0 +1,15 @@
+public interface saveInterface {
+
+    public void getFilepath();
+
+    public void setFilepath();
+
+    public void createTextFile();
+
+    public void overrideOldTextFile();
+
+    public void getName();
+
+    public void setName();
+
+}
