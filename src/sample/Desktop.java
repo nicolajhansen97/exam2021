594cb3742f03a7359a22f0e8bdba513808a3133f
NillerPog <niller1496@gmail.com>
2021-06-01 10:58:58 +0200

    Implements desktop interface

--- src/sample/Desktop.java
+++ src/sample/Desktop.java
@@ -1,4 +1,23 @@
 package sample;
 
 public class Desktop implements DesktopInterface{
+    @Override
+    public void createStickyNotes() {
+
+    }
+
+    @Override
+    public void searchStickyNotes(String searchCriteria) {
+
+    }
+
+    @Override
+    public void loadDesktopProject() {
+
+    }
+
+    @Override
+    public void saveStickyNotes() {
+
+    }
 }
