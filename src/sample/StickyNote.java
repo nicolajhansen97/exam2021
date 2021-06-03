3780f7b457376b2066725aeae1833c4cca701eb6
nicolajhansen97 <nicolajhansen97@live.dk>
2021-06-03 15:04:34 +0200

    A lot of small edits and added a menu.

--- src/sample/StickyNote.java
+++ src/sample/StickyNote.java
@@ -1,13 +1,10 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class StickyNote implements StickyNoteInterface {
