8a73757d571d65728bc4ee7105476e4074821f83
nicolajhansen97 <nicolajhansen97@live.dk>
2021-06-09 11:18:43 +0200

    Fixes and clean up

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Database {
 
+    private String password = "datamatiker";
 
     private ArrayList<StickyNote> tempStickyNote = new ArrayList<>();
 
@@ -17,12 +18,10 @@ public class Database {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
         // (2) establish Connection
-        // Connection con= DriverManager.getConnection("jdbc:sqlserver://EASV-THA-Q418\TH:1433;databaseName=DB_JAN","tha","123456");
-        Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa","123456");
+        Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa",password);
 
 
         // (3) create the statement
-        // Statement stmt = con.createStatement();
             String sql = "SELECT * FROM tblNotes";
             Statement stmt = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery(sql);
@@ -36,12 +35,8 @@ public class Database {
         // (3a) Prepare Statement
         PreparedStatement ps = con.prepareStatement("INSERT INTO tblNotes VALUES (?,?,?,?,?,?)");
 
-
-
-            // PreparedStatement ps = con.prepareStatement("SELECT * FROM project");
         for (int i = 0; i < StickyListSingleton.getInstance().getArray().size(); i++) {
 
-            //ps.setInt(1,StickyListSingleton.getInstance().getArray().get(i).getID());
             ps.setString(1,pName);
             ps.setString(2,StickyListSingleton.getInstance().getArray().get(i).getColorString());
             ps.setString(3,StickyListSingleton.getInstance().getArray().get(i).getSomeText());
@@ -53,32 +48,6 @@ public class Database {
         }
 
             System.out.println("Saved in database");
-
-          /*
-            ResultSet rs = ps.executeQuery();
-
-            ResultSetMetaData rsmd = rs.getMetaData();
-
-            int columns = rsmd.getColumnCount();
-                    // Project p = new Project();
-            while(rs.next()){
-
-                for (int num = 1; num <= columns; num++){
-                    System.out.print(rs.getString(num));
-                }
-                System.out.println();
-
-                // process data field by field
-
-
-            }
-            */
-
-
-        // (4) execute the SQL query
-        //int rows = stmt.executeUpdate("INSERT INTO project VALUES ('a1','Venus',56987)");
-
-        // System.out.println("number of rows affected = "+rows);
 
         // (5) close the connection
         ps.close();
@@ -96,7 +65,7 @@ public class Database {
         try{
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-            Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa","123456");
+            Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa",password);
 
             int count = 0;
             String sql = "SELECT * FROM tblNotes";
@@ -130,49 +99,16 @@ public class Database {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
             // (2) establish Connection
-            // Connection con= DriverManager.getConnection("jdbc:sqlserver://EASV-THA-Q418\TH:1433;databaseName=DB_JAN","tha","123456");
-            Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa","123456");
+            Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa",password);
 
             // (3a) Prepare Statement
             PreparedStatement ps = con.prepareStatement("INSERT INTO tblBoard VALUES (?, ?)");
-
-
-
-            // PreparedStatement ps = con.prepareStatement("SELECT * FROM project");
 
             ps.setString(1,projectName);
             ps.setInt(2,1);
 
             ps.executeUpdate();
             System.out.println("Saved in database");
-
-              /*
-                ResultSet rs = ps.executeQuery();
-
-                ResultSetMetaData rsmd = rs.getMetaData();
-
-                int columns = rsmd.getColumnCount();
-                        // Project p = new Project();
-                while(rs.next()){
-
-                    for (int num = 1; num <= columns; num++){
-                        System.out.print(rs.getString(num));
-                    }
-                    System.out.println();
-
-                    // process data field by field
-
-
-                }
-                */
-
-
-            // (4) execute the SQL query
-            //int rows = stmt.executeUpdate("INSERT INTO project VALUES ('a1','Venus',56987)");
-
-            // System.out.println("number of rows affected = "+rows);
-
-            // (5) close the connection
             ps.close();
             con.close();
 
@@ -185,7 +121,7 @@ public class Database {
         try{
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-            Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa","123456");
+            Connection con= DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=stickyNotesProgram","sa",password);
 
             String sql = "SELECT * FROM tblBoard";
             Statement stmt = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
