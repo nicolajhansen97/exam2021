62e06336727baacf85661d751f3d55af5a5cf92f
nicolajhansen97 <nicolajhansen97@live.dk>
2021-06-16 14:48:59 +0200

    Fixes

--- src/sample/Controller/LoadProjects.java
+++ src/sample/Controller/LoadProjects.java
@@ -22,16 +22,29 @@ public class LoadProjects {
       loadProjects();
     }
 
+    /**
+     * This function calls the database and make it load the project that is saved. Then it add everything to the list view,
+     * so you easily can pick the project you want on the list and load it.
+     */
     public void loadProjects(){
         database.loadProjects();
         lProjectsView.getItems().addAll(database.getTempProject());
     }
 
+    /**
+     * This gets the singleton instance and get the names from the projects. It is used to give the names to other classes
+     * @param actionEvent
+     */
     public void getProjectName(ActionEvent actionEvent) {
         ProjectNameSingleton.getInstance().setS((String) lProjectsView.getSelectionModel().getSelectedItem());
         Controller.stageLoadProjects.close();
     }
 
+    /**
+     * This delete the project from the listview, but also from the database. So its delete the project chosed on the listview,
+     * in the database, and after that it delete it from the database.
+     * @param actionEvent
+     */
     public void deleteProject(ActionEvent actionEvent) {
         System.out.println(lProjectsView.getSelectionModel().getSelectedItem().toString());
         database.deleteProject(lProjectsView.getSelectionModel().getSelectedItem().toString());
